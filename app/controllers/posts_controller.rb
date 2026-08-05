67569819db6bd04a70f9f5c6d28b1de98e013303
Yi <yizheng0118@gmail.com>
2026-08-05 17:14:37 -0500

    added actions new and create for Posts and requires auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,30 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
   def index
     @posts = Post.all
     @user = current_user
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Can't post empty post"
+      render :new, status: :unprocessable_entity
+    end
+  end
+private
+  def post_params
+    params.expect(post: [:content])
+  end
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "Login required"
+      redirect_to new_user_session_path
+    end
   end
 end
